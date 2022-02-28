f5660590a69bda1dd77a4a099895092684b3769f
Martin Günther <martin.guenther@dfki.de>
2022-02-28 17:31:42 +0100

    Fix shellcheck warnings

--- robot_api_up_demo/install-deps.sh
+++ robot_api_up_demo/install-deps.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-export FILEPATH=$(dirname `realpath "${BASH_SOURCE[0]}"`)
-cd $FILEPATH/../..
+FILEPATH=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+cd "$FILEPATH"/../.. || exit
 
 # Install dependencies.
-vcs import < $FILEPATH/my.repos
+vcs import < "$FILEPATH"/my.repos
 
 # Also install mobipick's dependencies.
 mobipick/install-deps.sh
@@ -13,5 +13,5 @@ pip install unified-planning/
 pip install up-pyperplan/
 
 # Checkout working commit in geometric_shapes repository as workaround.
-cd geometric_shapes/
+cd geometric_shapes/ || exit
 git checkout ca019f4
