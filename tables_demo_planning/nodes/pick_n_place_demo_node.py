f4d1946d9ab72612c94a61afe5c0baac9d6e8e03
Alexander Sung <alexander.sung@dfki.de>
2022-06-28 12:17:51 +0200

    Fix names in pick_n_place_demo

--- tables_demo_planning/nodes/pick_n_place_demo_node.py
+++ tables_demo_planning/nodes/pick_n_place_demo_node.py
@@ -14,7 +14,7 @@ class PickAndPlaceRobot(Robot):
         self.arm.execute("HasAttachedObjects")
         return Item.power_drill if self.arm.get_result().result else Item.nothing
 
-    def pick(self) -> bool:
+    def pick_power_drill(self) -> bool:
         self.arm.execute("CaptureObject")
         self.arm_pose = self.get_arm_pose()
         self.arm.execute("PickUpObject")
@@ -25,9 +25,9 @@ class PickAndPlaceRobot(Robot):
         self.item = Item.power_drill
         return True
 
-    def place(self) -> bool:
+    def place_power_drill(self) -> bool:
         self.arm.execute("PlaceObject")
-        self.arm_pose = ArmPose.place_1
+        self.arm_pose = ArmPose.place
         self.item = Item.nothing
         return True
 
@@ -35,13 +35,13 @@ class PickAndPlaceRobot(Robot):
 class PickAndPlace(Domain):
     def __init__(self) -> None:
         super().__init__(PickAndPlaceRobot("mobipick"))
-        self.pick, (robot,) = self.create_action(PickAndPlaceRobot, PickAndPlaceRobot.pick)
+        self.pick, (robot,) = self.create_action(PickAndPlaceRobot, PickAndPlaceRobot.pick_power_drill)
         self.pick.add_precondition(Equals(self.robot_has(robot), self.nothing))
         self.pick.add_precondition(Equals(self.robot_at(robot), self.base_pick_pose))
         self.pick.add_precondition(Equals(self.robot_arm_at(robot), self.arm_pose_home))
         self.pick.add_effect(self.robot_has(robot), self.power_drill)
         self.pick.add_effect(self.robot_arm_at(robot), self.arm_pose_interaction)
-        self.place, (robot,) = self.create_action(PickAndPlaceRobot, PickAndPlaceRobot.place)
+        self.place, (robot,) = self.create_action(PickAndPlaceRobot, PickAndPlaceRobot.place_power_drill)
         self.place.add_precondition(Equals(self.robot_has(robot), self.power_drill))
         self.place.add_precondition(Equals(self.robot_at(robot), self.base_place_pose))
         self.place.add_precondition(Equals(self.robot_arm_at(robot), self.arm_pose_transport))
