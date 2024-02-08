2df78048a022c3c8df6d42094ce5cdc95cbf973a
Sebastian Stock <sebastian.stock@gmail.com>
2024-02-08 13:22:37 +0100

    Adapt uplexmo node to again generate a plan
    But execution is still not working because
    of an AssertionError.
    up_esb/exceptions.py", line 105, in process_action_result
        assert result.action_status == ActionNodeStatus.SUCCEEDED

--- tables_demo_planning/nodes/uplexmo_tables_demo_node.py
+++ tables_demo_planning/nodes/uplexmo_tables_demo_node.py
@@ -44,63 +44,53 @@ Development in progress.
 import sys
 import rospy
 import unified_planning
-from typing import Optional, Set
-from tables_demo_planning.mobipick_components import Location
-from tables_demo_planning.tables_demo_api import TablesDemoAPIDomain
+from typing import Optional, Set, Dict
+from tables_demo_planning.components import Item, Location
+from tables_demo_planning.tables_demo_api import TablesDemoAPI
 from tables_demo_planning.subplan_visualization import SubPlanVisualization
-from unified_planning.plans import Plan, ActionInstance
-from unified_planning.shortcuts import OneshotPlanner, Problem
-from unified_planning.engines import OptimalityGuarantee
-from up_esb.plexmo import SequentialPlanMonitor, SequentialPlanDispatcher
+from unified_planning.plans import ActionInstance
+from up_esb.plexmo import PlanMonitor, PlanDispatcher
 from unified_planning.model import UPState
 
 
 class TablesDemoOrchestrator:
-    def __init__(self) -> None:
-        self._domain = TablesDemoAPIDomain()
+    def __init__(self, item_locations: Dict[Item, Location]) -> None:
+        self._demo_api = TablesDemoAPI(item_locations)
         self.visualization = SubPlanVisualization()
         self.espeak_pub: Optional[rospy.Publisher] = None
         self._trigger_replanning = False  # Temporary solution until it is provided by dispatcher
 
-    def solve_problem(self, problem: Problem, planner_name: Optional[str] = None) -> Plan:
-        result = OneshotPlanner(
-            name=planner_name,
-            problem_kind=problem.kind,
-            optimality_guarantee=OptimalityGuarantee.SOLVED_OPTIMALLY,
-        ).solve(problem)
-        return result.plan
-
     def dispatch_cb(self, action: ActionInstance) -> bool:
-        action_name = f"{len(self._executed_actions) + 1} {self._domain.label(action)}"
+        action_name = f"{len(self._executed_actions) + 1} {self._demo_api.label(action)}"
 
         # check preconditions
-        self._domain.set_initial_values(self._domain.problem)
-        monitor = SequentialPlanMonitor(self._domain.problem)
-        state = UPState(self._domain.problem.initial_values)
+        self._demo_api.set_initial_values(self._demo_api.problem)
+        monitor = PlanMonitor(self._demo_api.problem)
+        state = UPState(self._demo_api.problem.initial_values)
         unsatisfied = monitor.check_preconditions(action, state)[1]
         if len(unsatisfied) > 0:
             print("Preconditions of action %s are not satisfied" % action_name)
             print("Unsatisfied preconditions: %s" % str(unsatisfied))
             print("State is: ")
-            for k, v in self._domain.problem.initial_values.items():
+            for k, v in self._demo_api.problem.initial_values.items():
                 print(f"  {str(k)} := {str(v)}")
             # return False # TODO commented out until issue with wrong state is resolved
 
         # Execute the action
         print("Dispatching " + action_name)
-        executable_action, parameters = self._domain.get_executable_action(action)
+        executable_action, parameters = self._demo_api.get_executable_action(action)
         result = executable_action(*parameters)
         self._executed_actions.add(action_name)
         if rospy.is_shutdown():
             return False
 
-        if self._domain.env.item_search:
+        if self._demo_api.env.item_search:
             # Check whether an obsolete item search invalidates the previous plan.
             if (
-                self._domain.env.believed_item_locations.get(self._domain.env.item_search, self._domain.anywhere)
-                != self._domain.anywhere
+                self._demo_api.env.believed_item_locations.get(self._demo_api.env.item_search, self._demo_api.anywhere)
+                != self._demo_api.anywhere
             ):
-                print(f"Search for {self._domain.env.item_search.name} OBSOLETE.")
+                print(f"Search for {self._demo_api.env.item_search.name} OBSOLETE.")
                 if self.visualization:
                     self.visualization.cancel(action_name)
                 self._trigger_replanning = True
@@ -116,16 +106,16 @@ class TablesDemoOrchestrator:
     def search_item(self, previous_action_name) -> object:
         try:
             # Search for item by creating and executing a subplan.
-            self._domain.set_initial_values(self._domain.subproblem)
-            self._domain.set_search_goals()
-            subactions = self._domain.solve(self._domain.subproblem)
-            assert subactions, f"No solution for: {self._domain.subproblem}"
+            self._demo_api.set_initial_values(self._demo_api.subproblem)
+            self._demo_api.set_search_goals()
+            subactions = self._demo_api.solve(self._demo_api.subproblem)
+            assert subactions, f"No solution for: {self._demo_api.subproblem}"
             print("- Search plan:")
             print('\n'.join(map(str, subactions)))
             if self.visualization:
                 self.visualization.set_actions(
                     [
-                        f"{len(self._executed_actions)}{chr(number)} {self._domain.label(subaction)}"
+                        f"{len(self._executed_actions)}{chr(number)} {self._demo_api.label(subaction)}"
                         for number, subaction in enumerate(subactions, start=97)
                     ],
                     preserve_actions=self._executed_actions,
@@ -134,14 +124,14 @@ class TablesDemoOrchestrator:
             print("- Search execution:")
             subaction_execution_count = 0
             for subaction in subactions:
-                executable_subaction, subparameters = self._domain.get_executable_action(subaction)
-                label = self._domain.label(subaction)
+                executable_subaction, subparameters = self._demo_api.get_executable_action(subaction)
+                label = self._demo_api.label(subaction)
                 subaction_name = f"{len(self._executed_actions)}{chr(subaction_execution_count + 97)} {label}"
                 print(subaction)
                 if self.visualization:
                     self.visualization.execute(subaction_name)
                 if self.espeak_pub:
-                    self.espeak_pub.publish(self._domain.label(subaction))
+                    self.espeak_pub.publish(self._demo_api.label(subaction))
                 # Execute search action.
                 result = executable_subaction(*subparameters)
                 subaction_execution_count += 1
@@ -153,8 +143,8 @@ class TablesDemoOrchestrator:
                         # Note: True result only means any subaction succeeded.
                         # Check if the search actually succeeded.
                         if (
-                            self._domain.env.item_search is None
-                            and len(self._domain.env.newly_perceived_item_locations) <= 1
+                            self._demo_api.env.item_search is None
+                            and len(self._demo_api.env.newly_perceived_item_locations) <= 1
                         ):
                             print("- Continue with plan.")
                             if self.visualization:
@@ -162,8 +152,8 @@ class TablesDemoOrchestrator:
                             result = "Success"
                             break
                         # Check if the search found another item.
-                        elif self._domain.env.newly_perceived_item_locations:
-                            self._domain.env.newly_perceived_item_locations.clear()
+                        elif self._demo_api.env.newly_perceived_item_locations:
+                            self._demo_api.env.newly_perceived_item_locations.clear()
                             print("- Found another item, search ABORTED.")
                             if self.visualization:
                                 self.visualization.cancel(subaction_name)
@@ -179,25 +169,29 @@ class TablesDemoOrchestrator:
             # Note: The conclude action at the end of any search always fails.
         finally:
             # Always end the search at this point.
-            self._domain.env.item_search = None
+            self._demo_api.env.item_search = None
         return result
 
-    def generate_and_execute_plan(self, target_location: Location) -> None:
-        print(
-            "Scenario: Mobipick shall bring the box with the multimeter"
-            f" inside to {self._domain.objects[target_location.name]}."
-        )
+    def generate_and_execute_plan(
+        self, goal_strs: Optional[str] = None, target_location: Optional[Location] = None
+    ) -> None:
+        if goal_strs:
+            self._demo_api.domain.set_goals_by_strs(self._demo_api.problem, goal_strs)
+        else:  # Standard tables demo
+            print(
+                "Scenario: Mobipick shall bring the box with the multimeter"
+                f" inside to {self._demo_api.domain.objects[target_location.name]}."
+            )
+            self._demo_api.domain.set_goals(self._demo_api.problem, list(item_locations.keys()), target_location)
+
         self._executed_actions: Set[str] = set()
-        # retries_before_abortion = self._domain.RETRIES_BEFORE_ABORTION  # FIXME Currently not used
-        # error_counts: Dict[str, int] = defaultdict(int) # FIXME Currently not used
 
         self._trigger_replanning = True  # only for entering the loop
         while self._trigger_replanning:
             self._trigger_replanning = False
             # Plan
-            self._domain.set_initial_values(self._domain.problem)
-            self._domain.set_goals(target_location)
-            plan = self.solve_problem(self._domain.problem)
+            self._demo_api.domain.set_initial_values(self._demo_api.problem)
+            plan = self._demo_api.domain.solve(self._demo_api.problem)
             if not plan:
                 print("Could not find a plan. Exiting.")
                 return
@@ -208,9 +202,10 @@ class TablesDemoOrchestrator:
             # Execute the plan
             print("> Execution:")
 
-            dispatcher = SequentialPlanDispatcher()
+            dispatcher = PlanDispatcher()
             dispatcher.set_dispatch_callback(self.dispatch_cb)
-            dispatcher_result = dispatcher.execute_plan(plan)
+            graph = self._demo_api.domain.get_executable_graph(plan)
+            dispatcher_result = dispatcher.execute_plan(plan, graph)
             print(">Dispatcher finished with result " + str(dispatcher_result))
             if self._trigger_replanning:
                 print("Replanning")
@@ -218,16 +213,25 @@ class TablesDemoOrchestrator:
 
 if __name__ == '__main__':
     unified_planning.shortcuts.get_environment().credits_stream = None
+
+    # Define environment values.
+    item_locations = {
+        Item.get("multimeter_1"): Location.get("table_3"),
+        Item.get("klt_1"): Location.get("table_2"),
+        Item.get("klt_2"): Location.get("table_1"),
+        Item.get("klt_3"): Location.get("table_3"),
+    }
+
     try:
-        target_location = Location.table_2
-        if len(sys.argv) >= 2:
-            parameter = sys.argv[1]
-            if parameter in ("1", "table1", "table_1"):
-                target_location = Location.table_1
-            elif parameter in ("3", "table3", "table_3"):
-                target_location = Location.table_3
-            else:
-                rospy.logwarn(f"Unknown parameter '{parameter}', using default table.")
-        TablesDemoOrchestrator().generate_and_execute_plan(target_location)
+        # Define goal.
+        goal_strs = sys.argv[1:]
+        tdo = TablesDemoOrchestrator(item_locations)
+
+        if goal_strs:
+            tdo.generate_and_execute_plan(goal_strs)
+        else:  # Standard tables Demo goal
+            target_location = Location.get("table_2")
+            tdo.generate_and_execute_plan(goal_strs=None, target_location=target_location)
+
     except rospy.ROSInterruptException:
         pass
